0975a6449526a0598de389dcae4d98298c9e9053
MikeBish13 <michael.bishop13@gmail.com>
2021-03-24 10:06:17 +0000

    reload

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import './App.css';
-import InvoiceList from './components/InvoiceList';
-import InvoicePage from './components/InvoicePage';
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 
 
 function App() {
   return (
-    <Router >
-    <div className="App">
-      <Switch>
-      <Route path="/" exact component={InvoiceList}/>
-      <Route path="/invoice/:id" component={InvoicePage}/>
-      </Switch>
-    </div>
-    </Router>
+   <>
+   Hello
+   </>
   );
 }
 
