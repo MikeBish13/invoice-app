c7de0b668421dac9a04de1a6966828397fae495e
MikeBish13 <michael.bishop13@gmail.com>
2021-04-20 16:07:07 +0100

    fixed merge

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-import './styles/App.css';
-import InvoiceList from './components/InvoiceList';
-import InvoicePage from './components/InvoicePage';
-import NavBar from './components/NavBar';
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-=======
 import "./styles/App.css";
 import InvoiceList from "./Pages/InvoiceList";
 import { Route, Switch } from 'react-router-dom'
 import InvoicePage from "./Pages/InvoicePage";
 import {QueryClient, QueryClientProvider} from 'react-query';
 import SideBar from "./components/SideBar";
->>>>>>> development
 
 
 const queryClient = new QueryClient();
@@ -20,13 +12,6 @@ function App() {
   return (
     <>
     <div className="App">
-<<<<<<< HEAD
-      <NavBar />
-      <Switch>
-      <Route path="/" exact component={InvoiceList}/>
-      <Route path="/invoice/:id" component={InvoicePage}/>
-      </Switch>
-=======
     <QueryClientProvider client={queryClient}>
       <SideBar />
           <Switch>
@@ -34,7 +19,6 @@ function App() {
               <Route path='/invoice/:id' component={InvoicePage}></Route>
           </Switch>
     </QueryClientProvider>
->>>>>>> development
     </div>
     </>   
   );
--- src/storage/store.js
+++
@@ -1,9 +0,0 @@
-import create from 'zustand';
-
-export const useStore = create(set => ({
-    editStatus: false,
-    setEditStatus: () => set(state => ({editStatus: !state.editStatus})), 
-    addStatus: false,
-    setAddStatus: () => set(state => ({addStatus: !state.addStatus})) 
-}))
-
